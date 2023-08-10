a0451b13cd9a97102da57daeb3faac0d77bb40cb
JunYoung Lee <moderato85@gmail.com>
2023-08-10 14:48:44 +0900

    a

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import {Routes, Route } from 'react-router-dom';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path='/' element={<div>메인페이지</div>}/>
+        <Route path='/new' element={<div>새글페이지</div>}/>
+        <Route path='/edit' element={<div>수정페이지</div>}/>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
